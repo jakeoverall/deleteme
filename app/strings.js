a14e57e65b06f788b5c70dd17732ed44179a79a6
Jake Overall <joverall22@gmail.com>
2021-08-23 12:31:02 -0600

    solves letter at position

--- app/strings.js
+++ app/strings.js
@@ -1,10 +1,12 @@
 // Write a function that returns the character from a string at a given position
 function letterAtPosition(str, position) {
-
+    return str[position]
 }
 
 // Write a function that will join two strings with an optional seperating character 
 function concat(str1, str2, seperator = " ") {
+    console.log(`str1 = ${str1}  str2 = ${str2} seperator = ${seperator}`)
+    return 'hello worlds'
 }
 
 // Write a function that will capitalize the letter at a given position and return it
